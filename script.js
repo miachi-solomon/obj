4894323ec2f092a36af27d43a0f8142f46c9fccd
Miachi Solomon <smiachi10@gmail.com>
2025-07-01 15:24:50 +0100

    giving each instance an ID

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 }
 
 function addBookToLibrary (...args) {
     const book = new Book(args[0], args[1], args[2], args[3]);
     myLibrary.push(book);
 }
+
+addBookToLibrary('system', 'miachi', 45, false);
