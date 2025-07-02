efb3b976f4a300f93774804d35aca4d23f16a9f2
Miachi Solomon <smiachi10@gmail.com>
2025-07-02 14:29:32 +0100

    adding and styling generated html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const pageDiv = document.querySelector('.container');
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -8,9 +10,26 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary (...args) {
+function addBookToLibrary(...args) {
     const book = new Book(args[0], args[1], args[2], args[3]);
     myLibrary.push(book);
 }
 
-addBookToLibrary('system', 'miachi', 45, false);
+function displayBook() {
+    myLibrary.map((book) => {
+        let bookDiv = document.createElement('div');
+        const html = `
+        ${book.title}
+        ${book.author}
+        ${book.pages}
+        ${book.read}`;
+        bookDiv.classList.add('book-card');
+        bookDiv.append(html)
+        pageDiv.append(bookDiv);
+    });
+}
+
+addBookToLibrary('system design', 'miachi', 23, true);
+addBookToLibrary('scalable system', 'solomon', 45, false);
+
+displayBook()
